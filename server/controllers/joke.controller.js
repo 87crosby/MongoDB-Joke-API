3669fafe6f6082ee3dc6479f1fdf97aaf662d795
87crosby <71791452+87crosby@users.noreply.github.com>
2021-09-16 11:39:48 -0400

    Add files via upload

---
+++ server/controllers/joke.controller.js
@@ -0,0 +1,61 @@
+const Joke = require("../models/joke.model");
+
+
+
+module.exports.findAllJokes = (req,res)=>{
+    Joke.find()
+        .then(allJokes=>{
+            res.json({results: allJokes})
+        })
+        .catch(err=>{
+            res.json({err:err})
+        })
+}
+
+module.exports.findOneJoke = (req,res)=>{
+    
+    Joke.findOne({_id:req.params.id})
+        .then(foundJoke=>{
+            res.json({results: foundJoke })
+        })
+        .catch(err=>{
+            res.json({err:err})
+        })
+}
+
+
+module.exports.createNewJoke = (req,res)=>{
+    Joke.create(req.body)
+        .then(newJokeObj=>{
+            res.json({results: newJokeObj })
+        })
+        .catch(err=>{
+            res.json({err:err})
+        })
+}
+
+
+module.exports.updateExistingJoke = (req, res) => {
+    Joke.findOneAndUpdate(
+        { _id: req.params.id }, //find the objects whose _id == req.params.id
+        req.body, //req.body is the information from the form to update with
+        { new: true, runValidators: true } //new:true means return the newly updated info. 
+    )
+        .then(updatedJoke => {
+            res.json({ results: updatedJoke })
+        })
+        .catch(err=>{
+            res.json({err:err})
+        })
+        
+}
+
+module.exports.deleteJoke = (req,res)=>{
+    Joke.deleteOne({_id: req.params.id})
+        .then(deletedJoke =>{
+            res.json({results: deletedJoke})
+        })
+        .catch(err=>{
+            res.json({err:err})
+        })
+}
